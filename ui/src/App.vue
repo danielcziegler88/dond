1fdbd5a2e0db69dc10340531be7d95eaaa6d2a51
Daniel Ziegler <dziegler@vmware.com>
2021-01-03 15:00:21 +0000

    Starting Build

---
+++ ui/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    DOND
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "app",
+  components: {},
+  data: function() {
+    return {};
+  },
+  props: {},
+  watch: {},
+  methods: {},
+  computed: {},
+  mounted() {}
+};
+</script>
+
+<style>
+body {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
